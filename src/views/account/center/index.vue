2510a7b5df16404a00a4525b2be171aeab4eb4ea
jiangYF <770586208@qq.com>
2021-06-18 10:53:18 +0800

    更改个性签名

--- src/views/account/center/index.vue
+++ src/views/account/center/index.vue
@@ -8,7 +8,7 @@
               <img :src="avatar">
             </div>
             <div class="username">{{ nickname }}</div>
-            <div class="bio">海纳百川，有容乃大</div>
+            <div class="bio">钱旭傻逼</div>
           </div>
           <div class="account-center-detail">
             <p>
